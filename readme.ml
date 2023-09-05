bb52da8573f86f0fe1e00441cc309adbcc54831d
whr <whr@keba.com>
2023-09-05 17:15:10 +0200

    Added basic readme in markup language

---
+++ readme.ml
@@ -0,0 +1,17 @@
+
+Project: Temperature PWM Fan Control
+====================================
+
+Purpose:
+------------------------------------
+
+The goal of the project is to create a software and hardware to control standard 4-wire fans based on temperature sensors.
+
+Basic features:
+------------------------------------
+
+* microcontroller with pwm outout for three fans
+* multiple temperature sensors via I2C
+* rtc via i2c
+* serial interface for debugging and configuration
+
